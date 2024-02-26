df9fc23a04aef8641e186baefca633d5e38e37ed
Walid-Birouk <r0879039@student.thomasmore.be>
2024-02-26 01:14:37 +0100

    Updates

---
+++ component---src-pages-404-js-047a98307a2636ee7d62.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkWalid_Birouk=self.webpackChunkWalid_Birouk||[]).push([[125],{3331:function(e,t,n){n.r(t),n.d(t,{default:function(){return r}});var u=n(6540),l=n(1809);function r(){return u.createElement(l.A,null,u.createElement("div",null,u.createElement("h2",null,"404: Not found"),u.createElement("p",null,"Sorry, the page you requested doesn't exist")))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-047a98307a2636ee7d62.js.map
